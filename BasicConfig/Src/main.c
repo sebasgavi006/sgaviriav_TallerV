50d5e56584ad9a1a3dbf7a29e9b0877faae144ac
sebasgavi006 <sgaviriav@unal.edu.co>
2023-09-15 12:46:41 -0500

    Proyecto BasicConfig
    Creación y configuración del proyecto BasicConfig

---
+++ BasicConfig/Src/main.c
@@ -0,0 +1,26 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Sebastian Gaviria Valencia
+ * @brief          : Ejemplo de configuración básica que debe tener un proyecto
+ * 					 en el STM32 Cube IDE para que funcione correctamente y cumpla
+ * 					 las condiciones básicas necesarias que requiere un poryecto
+ * 					 de Taller V.
+ ******************************************************************************
+ **/
+
+#include <stdint.h>
+
+
+/* Función principal del programa. Es el corazón de todo */
+int main(void)
+{
+
+	/* Loop forever */
+	while(1){
+
+	}
+
+	return 0;	// Esta es una buena práctica de programaicón para que la función
+				// main retorne un valor trivial.
+}
