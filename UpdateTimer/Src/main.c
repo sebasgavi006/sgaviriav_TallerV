22515eac9e7831417a17cea425d8ebe3c70f648d
sebasgavi006 <sgaviriav@unal.edu.co>
2023-10-01 22:40:18 -0500

    Actualización Timer UpdateTimer
    Se actualiza el proyecto del Timer, utilizando el PeripheralsDrivers como proyecto que contiene las librerías

---
+++ UpdateTimer/Src/main.c
@@ -0,0 +1,71 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Sebastian Gaviria Valencia
+ * @brief          : Basic project, base for all new projects
+ ******************************************************************************
+ **/
+
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "stm32_assert.h"
+#include "gpio_driver_hal.h"
+#include "timer_driver_hal.h"
+
+// Se define un pin de prueba
+GPIO_Handler_t userLed = {0}; // PinA5
+
+Timer_Handler_t blinkTimer = {0};
+
+
+/* Función principal del programa */
+int main(void)
+{
+
+	/* Configurando el pin */
+	userLed.pGPIOx								= GPIOA;
+	userLed.pinConfig.GPIO_PinNumber			= PIN_5;
+	userLed.pinConfig.GPIO_PinMode				= GPIO_MODE_OUT;
+	userLed.pinConfig.GPIO_PinOutputType		= GPIO_OTYPE_PUSHPULL;
+	userLed.pinConfig.GPIO_PinOutputSpeed		= GPIO_OSPEED_MEDIUM;
+	userLed.pinConfig.GPIO_PinPuPdControl		= GPIO_PUPDR_NOTHING;
+
+	/* Cargamos la configuración del pin */
+	gpio_Config(&userLed);
+	gpio_WritePin(&userLed, SET);
+
+	/* Configurando el Timer */
+	blinkTimer.pTIMx								= TIM2;
+	blinkTimer.TIMx_Config.TIMx_Prescaler			= 16000;	// Genera incrementos de 1 ms
+	blinkTimer.TIMx_Config.TIMx_Period				= 250;		// De la mano con el pre-scaler, determina cuando se dispara una interrupción (250 ms)
+	blinkTimer.TIMx_Config.TIMx_mode				= TIMER_UP_COUNTER;	// El Timer cuante ascendente
+	blinkTimer.TIMx_Config.TIMx_InterruptEnable		= TIMER_INT_ENABLE;	// Se activa la interrupción
+
+	/* Cargamos la configuración del Timer */
+	timer_Config(&blinkTimer);
+
+	/* Encendemos el Timer */
+	timer_SetState(&blinkTimer, TIMER_ON);
+
+
+	/* Loop forever */
+	while(1){
+
+	}
+
+}
+
+
+/*
+ * Overwiter function
+ */
+void Timer2_Callback(void){
+	gpio_TooglePin(&userLed);
+}
+
+/* Función assert para detectar problemas de paŕametros incorrectos */
+void assert_failed(uint8_t* file, uint32_t line){
+	while(1){
+		// problems...
+	}
+}
